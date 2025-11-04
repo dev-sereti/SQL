ea0f920fd1115a0dfe8f636baefeaf2be56e1120
dev-sereti <devsereti@gmail.com>
2025-11-04 12:34:50 +0300

    Insert values into Books table

--- SQL.session.sql
+++ SQL.session.sql
@@ -53,8 +53,8 @@ VALUES
 ('Things Fall Apart',3,'17 June 1958.','12-06-2015'),
 ('Pride and Prejudice',4,'28-01-1813','10-09-2020'),
 ('Sense and Sensibility',4,'30-10-1811','03-11-2020'),
-(' Great Expectations',5,),
-('A Tale of Two Cities',5,),
+(' Great Expectations',5,'1-12-1860','12-05-2022'),
+('A Tale of Two Cities',5,''),
 ('The Great Gatsby',6,),
 ('The Time of the Hero, Death in the Andes',7,)
 
