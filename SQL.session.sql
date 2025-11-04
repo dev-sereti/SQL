c457e500a4303f980103e38b3d726de6576d9924
dev-sereti <devsereti@gmail.com>
2025-11-04 11:07:46 +0300

    Table created

--- SQL.session.sql
+++ SQL.session.sql
@@ -3,6 +3,8 @@ USE personal_library;
 CREATE TABLE Books(
     BookNumber INT PRIMARY KEY AUTO_INCREEMENT,
     BookTitle VARCHAR(50) NOT NULL,
-    AuthorFirstName VARCHAR()
+    AuthorFirstName VARCHAR(10),
+    AuthorLastName VARCHAR(10),
+    IssueDate DATE
 
-)
+);
