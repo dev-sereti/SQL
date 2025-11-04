b051fc0527d150236853c788e3b7451cf987c82a
dev-sereti <devsereti@gmail.com>
2025-11-04 11:17:51 +0300

    Create Authors table.

--- SQL.session.sql
+++ SQL.session.sql
@@ -1,9 +1,17 @@
 USE personal_library;
 
 CREATE TABLE Books(
-    BookNumber INT PRIMARY KEY AUTO_INCREEMENT,
+    BookNumber INT PRIMARY KEY AUTO_INCREMENT,
     BookTitle VARCHAR(50) NOT NULL,
-    AuthorFirstName VARCHAR(10),
-    AuthorLastName VARCHAR(10),
+    AuthorId INT,
     IssueDate DATE
 );
+-- Create Authors table.
+
+CREATE TABLE Authors(
+    AuthorId INT PRIMARY KEY AUTO_INCREMENT,
+    AuthorFirstName VARCHAR(10),
+    AuthorLastName VARCHAR(10),
+    Country VARCHAR(50)
+)
+
